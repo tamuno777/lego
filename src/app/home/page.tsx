931cc4bad429b332fc2f72f18c1981ff1524ce02
tamuno777 <davisbeulah07@gmail.com>
2023-09-08 13:57:57 -0700

    media query

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React from 'react'
 import Hero from './sections/hero'
 import About from './sections/about'
--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -4,9 +4,9 @@ import styles from '../../styles/home/desktop.module.css'
 
 export default function Contact() {
   return (
-    <div style={{backgroundColor:"black",color:"aliceblue",padding:"20px"}}>
-        <div style={{display:"flex",justifyContent:"center",flexDirection:"row",padding:"30px 0"}}>
-        <div style={{textAlign:"center",display:"flex",justifyContent:"center",flexDirection:"column",gap:"20px",width:"500px",borderBottom:"1px solid gray",padding:"0 30px 40px 30px"}}>
+    <div className={styles.contact} style={{backgroundColor:"black",color:"aliceblue",padding:"20px"}}>
+        <div  style={{display:"flex",justifyContent:"center",flexDirection:"row",padding:"30px 0"}}>
+        <div  style={{textAlign:"center",display:"flex",justifyContent:"center",flexDirection:"column",gap:"20px",width:"500px",borderBottom:"1px solid gray",padding:"0 30px 40px 30px"}}>
             <h2>Subscribe to <span className={styles.span}>Ou</span>r Newsletter </h2>
             <p style={{flexWrap:"wrap"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio et quaerat esse necessitatibus officiis suscipit.</p>
             <div style={{display:"flex",justifyContent:"center"}}>
@@ -15,7 +15,7 @@ export default function Contact() {
             </div>
         </div>
         </div>
-        <div>
+        <div >
             <div style={{display:"flex",justifyContent:"center",flexDirection:"row",padding:"30px 0",gap:"30px",textAlign:"center"}}>
                 <div>
                     <h3 style={{padding:"10px 0"}}>Address</h3>
